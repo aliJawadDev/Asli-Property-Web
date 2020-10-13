8dff547afedc40afa4470812809ab257f9d2d08d
aliJawadDev <immalijawad@gmail.com>
2020-10-13 18:01:46 +0500

    Api Done

--- src/screens/MyAds.js
+++ src/screens/MyAds.js
@@ -1,94 +1,91 @@
 import React, { Component } from 'react'
 import '../App.css'
-import { MyNav,MyItemCard } from '../Components/MyComponents'
+import { MyNav, MyItemCard } from '../Components/MyComponents'
+import axios from 'axios';
+import { ToastsContainer, ToastsStore } from 'react-toasts';
+import LoadingOverlay from 'react-loading-overlay';
 
 export default class MyAds extends Component {
     constructor(Props) {
         super(Props);
         this.state = {
-            data: [
-                {
-                    id: 1,
-                    title: "Hi",
-                    price: "PKR:100",
-                    description: "Demo Desc",
-                    imageSrc: require('../images/ads/1.png'),
-                },
-                {
-                    id: 2,
-                    title: "Bye",
-                    price: "PKR:100",
-                    description: "Demo Desc",
-                    imageSrc: require('../images/ads/1.png'),
-                },
-                {
-                    id: 3,
-                    title: "Cya",
-                    price: "PKR:100",
-                    description: "Demo Desc",
-                    imageSrc: require('../images/ads/1.png'),
-                },
-                {
-                    id: 4,
-                    title: "Welcome",
-                    price: "PKR:100",
-                    description: "Demo Desc",
-                    imageSrc: require('../images/ads/1.png'),
-                },
-                {
-                    id: 5,
-                    title: "Hi1",
-                    price: "PKR:100",
-                    description: "Demo Desc",
-                    imageSrc: require('../images/ads/1.png'),
-                },
-                {
-                    id: 6,
-                    title: "Bye2",
-                    price: "PKR:100",
-                    description: "Demo Desc",
-                    imageSrc: require('../images/ads/1.png'),
-                },
-                {
-                    id: 7,
-                    title: "Cya3",
-                    price: "PKR:100",
-                    description: "Demo Desc",
-                    imageSrc: require('../images/ads/1.png'),
-                },
-                {
-                    id: 8,
-                    title: "Welcome4",
-                    price: "PKR:100",
-                    description: "Demo Desc",
-                    imageSrc: require('../images/ads/1.png'),
-                },
-            ]
+            myAds: [],
+            favorite: [],
+            user: {},
+            loading: true,
         }
+    }
+
+    componentDidMount() {
+        var user = JSON.parse(localStorage.getItem('User'));
+        var id = user._id;
+        this.getUser(id);
+    }
+    getUser(id) {
+        axios.get(`https://property12.herokuapp.com/api/user/get/` + id)
+            .then(response => {
+                if (response.data.statusCode === 200) {
+                    var data = response.data.data;
+                    this.setState({ user: data })
+                    this.getMyAds();
+                    this.getfavorite();
+                }
+            }).catch(error => {
+                if (error) {
+                this.setState({loading:false})
+                    ToastsStore.error("Error\nCould not find user!");
+                }
+            });
+    }
+
+    getMyAds() {
+        axios.get(`https://property12.herokuapp.com/api/banner/get_phone/` + this.state.user.phoneNo)
+            .then(response => {
+                var data = response.data.data
+                this.setState({ myAds: data })
+            }).catch(error => {
+                if (error) {
+                this.setState({loading:false})
+                    ToastsStore.error("Error\nError getting Data!");
+                }
+            });
+    }
+    getfavorite() {
+        axios.get(`https://property12.herokuapp.com/api/favorite/get_phone/` + this.state.user.phoneNo)
+            .then(response => {
+                var data = response.data.data
+                this.setState({ favorite: data })
+                this.setState({loading:false})
+            }).catch(error => {
+                if (error) {
+                this.setState({loading:false})
+                    ToastsStore.error("Error\nError getting Data!");
+                }
+            });
     }
     render() {
         const styles = {
-            container:{
+            container: {
                 display: 'flex',
                 paddingTop: 70,
                 flex: 1,
                 minHeight: 100 + "vh",
             },
-            centerLine:{
+            centerLine: {
                 borderLeft: '2px solid white'
             },
-            title:{
+            title: {
                 fontWeight: 'bold',
                 color: 'white',
-                position:"sticky",
-                top:57,
-                backgroundColor:"#1390cf",
-                width:100+"%",
-                textAlign:'center',
-                padding:10,
-                borderBottom:'1px solid black'
+                position: "sticky",
+                top: 57,
+                backgroundColor: "#1390cf",
+                width: 100 + "%",
+                textAlign: 'center',
+                padding: 10,
+                borderBottom: '1px solid black'
             },
-            subCon:{
+            subCon: {
                 display: 'flex',
                 flex: 1,
                 flexDirection: 'column',
@@ -96,42 +93,50 @@ export default class MyAds extends Component {
             }
         };
         return (
-            <div style={styles.container}>
-                <MyNav />
-                <div style={styles.subCon}>
+            <LoadingOverlay
+                active={this.state.loading}
+                spinner
+                text='Fetching Data'>
+                <div style={styles.container}>
+                    <MyNav />
 
-                    <h2 style={styles.title}>My Ads</h2>
+                    <ToastsContainer store={ToastsStore} />
+
+                    <div style={styles.subCon}>
+
+                        <h2 style={styles.title}>My Ads</h2>
 
 
-                    {this.state.data.map(function (item) {
-                        return (
-                        <MyItemCard containerStyle={{ width: 18 + "em" }} title={item.title} toRoute={"/Item/" + item.id} price={item.price} description={item.description} imageSrc={item.imageSrc} />
-                        )
-                    })}
+                        {this.state.myAds.map(function (item) {
+                            return (
+                                <MyItemCard containerStyle={{ width: 18 + "em" }} title={item.title} toRoute={"/Item/" + item._id} price={item.price} description={item.description} imageSrc={item.image[0]} />
+                            )
+                        })}
 
+
+                    </div>
+
+
+                    <div style={styles.centerLine} />
+
+
+
+                    <div style={styles.subCon}>
+
+                        <h2 style={styles.title}>Favorites</h2>
+
+
+                        {this.state.favorite.map(function (item) {
+                            return (
+                                <MyItemCard containerStyle={{ width: 18 + "em" }} title={item.title} toRoute={"/Item/" + item._id} price={item.price} description={item.description} imageSrc={item.image[0]} />
+                            )
+                        })}
+
+
+                    </div>
 
                 </div>
-
-
-                <div style={styles.centerLine} />
-
-
-
-                <div style={styles.subCon}>
-
-                    <h2 style={styles.title}>Favorites</h2>
-
-
-                    {this.state.data.map(function (item) {
-                        return (
-                        <MyItemCard containerStyle={{ width: 18 + "em" }} title={item.title} toRoute={"/Item/" + item.id} price={item.price} description={item.description} imageSrc={item.imageSrc} />
-                        )
-                    })}
-
-                    
-                </div>
-
-            </div>
+            </LoadingOverlay>
         )
     }
 }
--- src/screens/Sell.js
+++ src/screens/Sell.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import '../App.css'
 import { MyInput, MyBtn, MyLogoText, MyNav } from '../Components/MyComponents'
+import axios from 'axios';
+import LoadingOverlay from 'react-loading-overlay';
+import { ToastsContainer, ToastsStore } from 'react-toasts';
+
 
 export default class Sell extends Component {
     constructor(Props) {
@@ -16,7 +20,62 @@ export default class Sell extends Component {
             description: "",
             proofOfOwnership: "",
             images: [],
+            userPhone: "",
+            working: false
         }
+    }
+    componentDidMount() {
+        var user = JSON.parse(localStorage.getItem('User'));
+        var id = user._id;
+        this.setState({ userPhone: user.phoneNo })
+        this.getUser(id);
+    }
+    getUser(id) {
+        axios.get(`https://property12.herokuapp.com/api/user/get/` + id)
+            .then(response => {
+                if (response.data.statusCode === 200) {
+                    var data = response.data.data;
+                    this.setState({ user: data })
+                }
+            }).catch(error => {
+                if (error) {
+                    ToastsStore.error("Error\nCould not find user!");
+                }
+            });
+    }
+    submitAd = () => {
+        let data = {
+            image: this.state.images,
+            title: this.state.title,
+            type: this.state.type,
+            price: this.state.price,
+            area: this.state.area,
+            purpose: this.state.purpose,
+            city: this.state.city,
+            location: this.state.location,
+            description: this.state.description,
+            phoneNo: this.state.userPhone
+        }
+        if (this.state.images.length && this.state.title.length && this.state.type.length && this.state.price.length && this.state.area.length && this.state.purpose.length && this.state.city.length && this.state.location.length && this.state.description.length && this.state.userPhone.length) {
+            this.setState({ working: true })
+            axios.post(`https://property12.herokuapp.com/api/banner/add`, data)
+                .then(response => {
+                    if (response.status === 200) {
+                        this.setState({ working: false })
+                        ToastsStore.success("Data Added Successfully!");
+                    }
+                }).catch(error => {
+                    if (error) {
+                        this.setState({ working: false })
+                        ToastsStore.error("Error\nTry Again Later!");
+                    }
+                });
+        }
+        else {
+            ToastsStore.error("Please Fill inputs!");
+        }
+
+
     }
     handleChange(e) {
         this.setState({ [e.target.name]: [e.target.value] })
@@ -35,61 +94,70 @@ export default class Sell extends Component {
             }
         };
         return (
-            <div style={{ paddingTop: 100 }}>
-                <MyNav />
-                <div className="sellCon">
+            <LoadingOverlay
+                active={this.state.working}
+                spinner
+                text='Uploading data'>
+                <div style={{ paddingTop: 100 }}>
+                    <MyNav />
 
-                    <MyLogoText text="Asli Property" imageSrc={require("../images/logo.png")} />
+                    <ToastsContainer store={ToastsStore} />
 
-                    <MyInput label="Title" placeHolder="Title here" inputStyle={styles.inputStyle} name="title" onChange={(e) => this.handleChange(e)} />
 
-                    <h5 style={{ fontWeight: 'bold', color: "#fff", marginTop: 10 }}>Type:</h5>
+                    <div className="sellCon">
 
-                    <label className="radioContainer">Plot
+                        <MyLogoText text="Asli Property" imageSrc={require("../images/logo.png")} />
+
+                        <MyInput label="Title" placeHolder="Title here" inputStyle={styles.inputStyle} name="title" onChange={(e) => this.handleChange(e)} />
+
+                        <h5 style={{ fontWeight: 'bold', color: "#fff", marginTop: 10 }}>Type:</h5>
+
+                        <label className="radioContainer">Plot
                         <input type="radio" value="Plot" onChange={(e) => this.handleChange(e)} name="type" />
-                        <span className="checkmark"></span>
-                    </label>
+                            <span className="checkmark"></span>
+                        </label>
 
-                    <label className="radioContainer">Home
+                        <label className="radioContainer">Home
                         <input type="radio" value="Home" onChange={(e) => this.handleChange(e)} name="type" />
-                        <span className="checkmark"></span>
-                    </label>
-                    <label className="radioContainer">Flat
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radioContainer">Flat
                         <input type="radio" value="Flat" onChange={(e) => this.handleChange(e)} name="type" />
-                        <span className="checkmark"></span>
-                    </label>
-                    <label className="radioContainer">Commercial
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radioContainer">Commercial
                         <input type="radio" value="Commercial" onChange={(e) => this.handleChange(e)} name="type" />
-                        <span className="checkmark"></span>
-                    </label>
+                            <span className="checkmark"></span>
+                        </label>
 
-                    <MyInput label="Price" placeHolder="Price here" inputStyle={styles.inputStyle} name="price" onChange={(e) => this.handleChange(e)} />
+                        <MyInput label="Price" placeHolder="Price here" inputStyle={styles.inputStyle} name="price" onChange={(e) => this.handleChange(e)} />
 
-                    <MyInput label="Area" placeHolder="Area here" inputStyle={styles.inputStyle} name="area" onChange={(e) => this.handleChange(e)} />
+                        <MyInput label="Area" placeHolder="Area here" inputStyle={styles.inputStyle} name="area" onChange={(e) => this.handleChange(e)} />
 
-                    <MyInput label="Purpose" placeHolder="Purpose here" inputStyle={styles.inputStyle} name="purpose" onChange={(e) => this.handleChange(e)} />
+                        <MyInput label="Purpose" placeHolder="Purpose here" inputStyle={styles.inputStyle} name="purpose" onChange={(e) => this.handleChange(e)} />
 
-                    <MyInput label="City" placeHolder="City here" inputStyle={styles.inputStyle} name="city" onChange={(e) => this.handleChange(e)} />
+                        <MyInput label="City" placeHolder="City here" inputStyle={styles.inputStyle} name="city" onChange={(e) => this.handleChange(e)} />
 
-                    <MyInput label="Purpose" placeHolder="Purpose here" inputStyle={styles.inputStyle} name="purpose" onChange={(e) => this.handleChange(e)} />
+                        <MyInput label="Purpose" placeHolder="Purpose here" inputStyle={styles.inputStyle} name="purpose" onChange={(e) => this.handleChange(e)} />
 
-                    <MyInput label="Location" placeHolder="Location here" inputStyle={styles.inputStyle} name="location" onChange={(e) => this.handleChange(e)} />
+                        <MyInput label="Location" placeHolder="Location here" inputStyle={styles.inputStyle} name="location" onChange={(e) => this.handleChange(e)} />
 
-                    <MyInput label="Description" placeHolder="Description here" textarea inputStyle={styles.inputStyle} name="description" onChange={(e) => this.handleChange(e)} />
+                        <MyInput label="Description" placeHolder="Description here" textarea inputStyle={styles.inputStyle} name="description" onChange={(e) => this.handleChange(e)} />
 
-                    <h5 style={{ fontWeight: 'bold', color: "#fff" }}>Proof of ownership:</h5>
+                        <h5 style={{ fontWeight: 'bold', color: "#fff" }}>Proof of ownership:</h5>
 
-                    <MyInput inputType="file" imageOnly inputStyle={styles.imageInput} inputClass="" name="proofOfOwnership" onChange={(e) => this.handleChange(e)} />
+                        <MyInput inputType="file" imageOnly inputStyle={styles.imageInput} inputClass="" name="proofOfOwnership" onChange={(e) => this.handleChange(e)} />
 
-                    <h5 style={{ fontWeight: 'bold', color: "#fff" }}>Property Images:</h5>
+                        <h5 style={{ fontWeight: 'bold', color: "#fff" }}>Property Images:</h5>
 
-                    <MyInput multiple imageOnly inputType="file" inputStyle={styles.imageInput} name="images" onChange={(e) => this.handleChange(e)} />
+                        <MyInput multiple imageOnly inputType="file" inputStyle={styles.imageInput} name="images" onChange={(e) => this.handleChange(e)} />
 
-                    <MyBtn startColor="#13c0cf" className="HoverEffect" endColor="#1390cf" style={{ color: 'white' }} title="Sell" onClick={() => alert("SOLD")} />
+                        <MyBtn startColor="#13c0cf" className="HoverEffect" endColor="#1390cf" style={{ color: 'white' }} title="Sell" onClick={() => this.submitAd()} />
+
+                    </div>
 
                 </div>
-
-            </div>
+            </LoadingOverlay>
         )
     }
 }
